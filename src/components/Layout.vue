e14a3766dd8e27f4e6dd645e3c603944616164c0
DekiRostilj7 <dejan.rosulj89@gmail.com>
2019-11-26 20:05:32 +0100

    First task finished

---
+++ src/components/Layout.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+      <div>
+        <nav>
+            <ul>
+                <li>
+                    <router-link to="Cars">Cars</router-link>
+                </li>
+            </ul>
+        </nav>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,22 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import VueRouter from 'vue-router'
+
 Vue.config.productionTip = false
+Vue.use(VueRouter);
+
+const routes = [
+  {name:'Cars', path:'/cars'}
+]
+
+const router = new VueRouter({
+  routes,
+  linkExactActiveClass: "link-active",
+  mode: "history"
+})
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
